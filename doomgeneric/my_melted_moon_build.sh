899d8cf59d4dd04b527ccd2bcdb774c50fccd362
fl4shk <fl4shk@users.noreply.github.com>
2026-09-03 13:59:15 -0500

    fix bug in `my_melted_moon_build.sh`

--- doomgeneric/my_melted_moon_build.sh
+++ doomgeneric/my_melted_moon_build.sh
@@ -17,12 +17,12 @@ make -f Makefile.melted_moon_riscv32 -j8 all \
     && make -f Makefile.melted_moon_riscv32 -j8 dasm \
     && make DO_TIMEDEMO_3=1 -f Makefile.melted_moon_riscv32 -j8 all \
     && make DO_TIMEDEMO_3=1 -f Makefile.melted_moon_riscv32 -j8 dasm \
-    && make DO_POLLINFO_3=1 -f Makefile.melted_moon_riscv32 -j8 all \
+    && make DO_POLLINFO=1 -f Makefile.melted_moon_riscv32 -j8 all \
     && make DO_POLLINFO=1 -f Makefile.melted_moon_riscv32 -j8 dasm \
     && make DO_TIMEDEMO_3=1 DO_POLLINFO=1 -f Makefile.melted_moon_riscv32 -j8 all \
     && make DO_TIMEDEMO_3=1 DO_POLLINFO=1 -f Makefile.melted_moon_riscv32 -j8 dasm
 
 #make -f Makefile.melted_moon_riscv32 -j8 all \
 #    && make DO_TIMEDEMO_3=1 -f Makefile.melted_moon_riscv32 -j8 all \
-#    && make DO_POLLINFO_3=1 -f Makefile.melted_moon_riscv32 -j8 all \
+#    && make DO_POLLINFO=1 -f Makefile.melted_moon_riscv32 -j8 all \
 #    && make DO_TIMEDEMO_3=1 DO_POLLINFO=1 -f Makefile.melted_moon_riscv32 -j8 all \
